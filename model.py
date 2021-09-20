10af1a7d6ee40614d2bcb1ae8708e54c691b6cce
HaoWuLab-Bioinformatics <55370215+HaoWuLab-Bioinformatics@users.noreply.github.com>
2021-09-20 10:48:08 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -28,19 +28,64 @@ x_val = np.array(f['x_val'])
 y_train = np.array(f['y_train'])
 y_test = np.array(f['y_test'])
 y_val= np.array(f['y_val'])
-x = np.loadtxt('k-mer_array_k=6.txt')
+
 a = np.hstack((y_train,y_val))
 y = np.hstack((a,y_test))
+'''
+x1 = np.loadtxt('feature_k-mers_k=6.txt')
+x2 = np.loadtxt('feature_NPSE_k=9.txt')
+x3 = np.loadtxt('feature_PSSM.txt')
+x4 = np.loadtxt('feature_Mismatch_k=3.txt')
+x5 = np.loadtxt('feature_subsequence_profile_k=3_delta=0.5.txt')
+x6 = np.loadtxt('feature_psednc.txt')
+x7 = np.loadtxt('feature_NV_k=1.txt')
+'''
+def noramlization(data):
+    minVals = data.min(0)
+    maxVals = data.max(0)
+    ranges = maxVals - minVals
+    normData = np.zeros(np.shape(data))
+    m = data.shape[0]
+    normData = data - np.tile(minVals, (m, 1))
+    normData = normData/np.tile(ranges, (m, 1))
+    return normData
+
+
+x = np.loadtxt('feature_k-mers_k=6.txt')
+x = noramlization(x)
+'''x1 = np.loadtxt('feature_k-mers_k=6.txt')
+x1 = noramlization(x1)
+x2 = np.loadtxt('feature_NPSE_k=7.txt')
+x2 = noramlization(x2)
+x3 = np.loadtxt('feature_Mismatch_k=8.txt')
+x3 = noramlization(x3)
+x4 = np.loadtxt('feature_subsequence_profile_k=6_delta=0.5.txt')
+x4 = noramlization(x4)
+x5 = np.loadtxt('feature_PSSM.txt')
+x5 = noramlization(x5)
+x6 = np.loadtxt('feature_psednc.txt')
+x6 = noramlization(x6)
+x7 = np.loadtxt('feature_NV_k=1.txt')
+x7 = noramlization(x7)'''
+
+
+
+
+print(x.shape)
 print('load over')
+x_train,x_test, y_train, y_test = train_test_split(x,y,test_size=0.2, random_state=10)
 
-
-model1 = LogisticRegression(random_state=10, max_iter=5000)
-model2 = KNeighborsClassifier(n_neighbors=1)
+x_train,_, y_train, _ = train_test_split(x_train,y_train,test_size=0.5, random_state=10)
+print(x_train.shape)
+print(x_test.shape)
+model1 = LogisticRegression(random_state=10, max_iter=50000,penalty='l2', C=1.0)
+model2 = KNeighborsClassifier(n_neighbors=1,leaf_size=30, p=2)
 model3 = RandomForestClassifier(n_estimators = 300, random_state=10)
-model4 = svm.SVC(random_state=10,probability=True)
+model4 = svm.SVC(random_state=10,probability=True,gamma = 'scale',C=1.0,kernel= 'rbf')
 
 stack = StackingCVClassifier(
     classifiers=[model1, model2, model3], meta_classifier=model4, random_state=10,use_probas=True,cv=5)
+
 
 kf = KFold(10, True, 10)
 i=0
@@ -50,17 +95,17 @@ mcc=np.zeros(10)
 precision=np.zeros(10)
 recall=np.zeros(10)
 fscore=np.zeros(10)
-for train_index, test_index in kf.split(x):
-    stack.fit(x[train_index], y[train_index])
-    stack_pred = stack.predict_proba(x[test_index])
+for train_index, test_index in kf.split(x_train):
+    stack.fit(x_train[train_index], y_train[train_index])
+    stack_pred = stack.predict_proba(x_train[test_index])
     stack_predict = stack_pred[:, 1]
-    stack_p = stack.predict(x[test_index])
-    acc_stack[i] = accuracy_score(y[test_index], stack_p)
+    stack_p = stack.predict(x_train[test_index])
+    acc_stack[i] = accuracy_score(y_train[test_index], stack_p)
 
-    fpr, tpr, thresholdTest = roc_curve(y[test_index], stack_predict)
+    fpr, tpr, thresholdTest = roc_curve(y_train[test_index], stack_predict)
     aucv[i] = auc(fpr, tpr)
-    mcc[i] = matthews_corrcoef(y[test_index], stack_p)
-    precision[i],recall[i],fscore[i],support=precision_recall_fscore_support(y[test_index],stack_p,average='macro')
+    mcc[i] = matthews_corrcoef(y_train[test_index], stack_p)
+    precision[i],recall[i],fscore[i],support=precision_recall_fscore_support(y_train[test_index],stack_p,average='macro')
 
     i = i + 1
     print('auc:', aucv)
@@ -69,10 +114,33 @@ for train_index, test_index in kf.split(x):
     print('precision:', precision)
     print('recall:', recall)
     print('fscore:', fscore)
-
+print('10-fold cross-validation on the training set')
 print('auc:', aucv.mean())
 print('acc:',acc_stack.mean())
 print('mcc:', mcc.mean())
 print('precision:', precision.mean())
 print('recall:', recall.mean())
 print('fscore:', fscore.mean())
+print(aucv.mean(),acc_stack.mean(),mcc.mean(),precision.mean(),recall.mean(),fscore.mean())
+
+
+stack.fit(x_train, y_train)
+stack_pred = stack.predict_proba(x_test)
+stack_predict = stack_pred[:, 1]
+stack_p = stack.predict(x_test)
+acc_stack_test = accuracy_score(y_test, stack_p)
+
+fpr, tpr, thresholdTest = roc_curve(y_test, stack_predict)
+aucv_test = auc(fpr, tpr)
+mcc_test = matthews_corrcoef(y_test, stack_p)
+precision_test, recall_test, fscore_test, support = precision_recall_fscore_support(y_test, stack_p,
+                                                                              average='macro')
+
+print('Evaluation on the test set')
+print('auc:', aucv_test)
+print('acc:',acc_stack_test)
+print('mcc:', mcc_test)
+print('precision:', precision_test)
+print('recall:', recall_test)
+print('fscore:', fscore_test)
+print(aucv_test,acc_stack_test,mcc_test,precision_test,recall_test,fscore_test)
